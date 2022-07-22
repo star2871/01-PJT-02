3bd59b31b3b6f002fb8b2099cff0153b8e0443a8
suyoung049 <suyoung049@gmail.com>
2022-07-22 22:26:52 +0900

    5번 풀이 및 후기 추가

--- 2회차/이수영/05.py
+++ 2회차/이수영/05.py
@@ -1,13 +1,46 @@
 import requests
 from pprint import pprint
 
-
 def credits(title):
-    pass 
-    # 여기에 코드를 작성합니다.  
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/search/movie'
+    params = {
+        'api_key':'dcc1e7ed069f7427a2816e92b32e7c8b',
+        'language': 'ko-KR',
+        'query': title
+        }
+    response = requests.get(BASE_URL+path, params = params).json()
+    movies = response.get('results')
+    if movies ==[]:
+        result = 'none'
+    else:
+        first_id = movies[0]['id']
+        path_2 = f'/movie/{first_id}/credits'
+        params = {
+            'api_key':'dcc1e7ed069f7427a2816e92b32e7c8b',
+            'language': 'ko-KR'
+            }
+        response = requests.get(BASE_URL+path_2, params = params).json()
+        casts = response.get('cast')
+        crews = response.get('crew')
+        main = []
+        for cast in casts:
+            if cast['cast_id'] < 10:
+                main.append(cast)
+        Fame = []
+        for act in main:
+            Fame.append(act['name'])
+        people = []
+        for crew in crews:
+            if crew['department'] == 'Directing':
+                people.append(crew)
+        emp = []
+        for person in people:
+            emp.append(person['name'])
+        all = {'cast': Fame, 'crew': emp}
+        return all
 
-
-# 아래의 코드는 수정하지 않습니다.
+        
 if __name__ == '__main__':
     """
     제목에 해당하는 영화가 있으면 해당 영화 id를 통해 영화 상세정보를 검색하여 주연배우 목록(cast)과 스태프(crew) 중 연출진 목록을 반환
