7c041f2aec1c6c2a97d0d95004831a07d7bbf16e
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-07-22 17:24:04 +0900

    1회차 최민아 프로젝트 풀이 및 후기

--- 1회차/최민아/03.py
+++ 1회차/최민아/03.py
@@ -3,8 +3,21 @@ from pprint import pprint
 
 
 def ranking():
-    pass 
-    # 여기에 코드를 작성합니다.  
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/movie/popular'
+    params = {
+        'api_key': '',
+        'language': 'ko-KR'
+    }
+
+    response = requests.get(BASE_URL+path, params=params).json()  
+    movies = response.get('results')
+    top_5 = []
+    
+    sorted_movies = sorted(movies, key=lambda x:x['vote_average'], reverse=True)
+    top_5 = sorted_movies[:5]
+    
+    return top_5  
 
 
 # 아래의 코드는 수정하지 않습니다.
