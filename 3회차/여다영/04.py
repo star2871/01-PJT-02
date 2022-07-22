ce5e5b9a7c845d5b8f7bcb983e3bcb41e8e3e739
yodayeong <duekdud01@naver.com>
2022-07-22 16:44:39 +0900

    여다영

--- 3회차/여다영/04.py
+++ 3회차/여다영/04.py
@@ -1,3 +1,17 @@
+#04. 영화 조회 및 추천 영화 조회
+
+#- 영화 제목으로 검색을 하여 추천 영화 목록을 출력합니다.
+#- requests 라이브러리를 활용하여 TMDB에서 영화제목으로 영화를 검색(Search Movies)합니다.
+#- 응답 받은 결과 중 첫번째 영화의 id 값을 활용하여 TMDB에서 추천 영화 목록(Get Recommendations)을 가져옵니다.
+#- 추천 영화 목록을 리스트로 반환하는 함수를 작성합니다.
+
+### 결과 예시
+#**요청 시점에 따라 다른 결과가 나올 수가 있습니다.**
+#["조커", "1917", "조조 래빗", "원스 어폰 어 타임 인… 할리우드", "... 생략" ,"펄프픽션"]
+
+#api_key : f4d88a36cfb682b86111c15f97a34324
+#https://api.themoviedb.org/3/movie/550?api_key=f4d88a36cfb682b86111c15f97a34324
+
 import requests
 from pprint import pprint
 
