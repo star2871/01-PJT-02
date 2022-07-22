5b70b8972832980e700c795bf52626f2b884b540
yujeong6676 <stellachoi6676@gmail.com>
2022-07-22 21:47:34 +0900

    최유정 03까지 PR

--- 3회차/최유정/01.py
+++ 3회차/최유정/01.py
@@ -1,9 +1,27 @@
+# tmdb api key값: 854ba974b2e0c5125edf366d30dc122b
+
+# 01. 인기 영화 조회
+# - 인기 영화 목록의 개수를 출력합니다.
+# - requests 라이브러리를 활용하여 TMDB에서 현재 인기 있는 영화 목록(Get Populations) 데이터를 요청합니다.
+# - 응답 받은 데이터 영화 개수를 반환하는 함수를 작성합니다.
 import requests
 
 
 def popular_count():
     pass 
     # 여기에 코드를 작성합니다.  
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/movie/popular'
+    params = {
+        'api_key' : '854ba974b2e0c5125edf366d30dc122b',
+        'language' : 'ko-KR'
+    }
+    response = requests.get(BASE_URL+path, params=params).json()
+    
+    #리스트의 개수 세기
+    result = len(response['results'])
+    return result
+
 
 
 # 아래의 코드는 수정하지 않습니다.
