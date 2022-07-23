86794559b87ebe7b62c2a1ab8c15d616124fac2a
kimheekimhee <khd128k@gmail.com>
2022-07-23 22:47:22 +0900

    python

--- 2회차/김희동/01.py
+++ 2회차/김희동/01.py
@@ -1,10 +1,19 @@
+from itertools import count
 import requests
-
+from pprint import pprint
 
 def popular_count():
-    pass 
-    # 여기에 코드를 작성합니다.  
-
+    pass
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/movie/popular'
+    params = {
+        'api_key' : '802f1efe43a0b0e486e74690ed6e97ae', 
+        'language' : 'ko-KR'}
+    response = requests.get(BASE_URL+path, params=params).json()
+    # pprint(response.keys())
+    # pprint(response['results'])
+    # pprint(len(response['results']))
+    return len(response['results'])
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
