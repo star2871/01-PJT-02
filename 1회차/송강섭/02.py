da6fcff428e26802730aefed6bbc2bc690029cdb
1c0332zz <1c0332zz@gmail.com>
2022-07-23 23:53:29 +0900

    1회차 송강섭

--- 1회차/송강섭/02.py
+++ 1회차/송강섭/02.py
@@ -1,16 +1,33 @@
+# - 인기 영화 목록 중 평점이 8점 이상인 영화 목록을 출력합니다.
+# - requests 라이브러리를 활용하여 TMDB에서 현재 인기 있는 영화 목록(Get Populations) 데이터를 요청합니다.
+# - 응답 받은 데이터 중 평점(`vote_average`)이 8점 이상인 영화 목록을 리스트로 반환하는 함수를 작성합니다.
+
 import requests
 from pprint import pprint
 
 
 def vote_average_movies():
-    pass 
-    # 여기에 코드를 작성합니다.  
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/movie/popular'
+    params = {
+    'api_key' : 'd2f94df713ea62aabe6d2d6157100a12',
+    'language' : 'ko-KR'
+    }
+    response = requests.get(BASE_URL+path, params=params).json()       
+    results = response.get('results')
+    # pprint(results)                                               # 여기까진 영화 목록 잘 작동
+    box = []                                                      # 이제 알았지만 append해줄려면 리스트여야 함 ''안댐
+    for i in results:                                             # 영화 목록을 돌려주면서 그중 vote_average만 빼내주기
+      if i.get('vote_average') >= 8:                              # 영화의 목록 중 vote_average가 8보다 크거나같으면 엎어주기 // 어디다가? 변수하나 만들어서?
+        box.append(i)                                             # 조건이 맞을 때 마다 변수 안에 i를 추가함
+    return box                                                    # 들여쓰기 해줘야함 꼭
+
 
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     """
-    popular 영화목록중 vote_average가 8 이상인 영화목록 반환
+    popular 영화목록중 vote_average가가 8 이상인 영화목록 반환
     (주의) popular 영화목록의 경우 시기에 따라 아래 예시 출력과 차이가 있을 수 있음
     """
     pprint(vote_average_movies())
--- 1회차/송강섭/04.py
+++ 1회차/송강섭/04.py
@@ -1,13 +1,39 @@
+# 영화제목으로 검색하여 추천 영화 목록 출력 (Search Movies)
+# 응답받은 결과 중 첫번째 영화의 id값을 활용해 TMDB에서 추천영화목록을 가져옴(Get Recommendations) = /movie/{movie_id}/recommendations
+# 추천 영화 목록을 리스트로 반환하는 함수를 작성
+
+# 검색할수 있는것과 id값을 활용해 목록을 만든다
+
 import requests
 from pprint import pprint
 
 
 def recommendation(title):
-    pass 
-    # 여기에 코드를 작성합니다.  
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/search/movie' # 영화의 제목으로 검색한다.
+    # /movie/{movie_id}/recommendations # 추천 영화목록
+    params = {
+        'api_key' : 'd2f94df713ea62aabe6d2d6157100a12',
+        'language' : 'ko-KR',
+        'query' : title # 제목이 필수 키이기 때문에 query를 추가해준다.
+    }
+    response = requests.get(BASE_URL+path, params).json()
+    results = [] # 결과를 출력하기 위한 리스트를 만들어줌
+    if response['results']:
+        movie_id = response['results'][0]['id'] # 이 문장이 곧 'id' : 496243 <<딕셔너리의 값을 나타냄.
+        path2 = f'/movie/{movie_id}/recommendations' # f를 입력해 {}안에 들어갈수있게해줌
+        params2 = {
+            'api_key': 'e3ebcaf0cb86336e3fa61579f1f0569b',
+            'language': 'ko-KR',
+            'query': movie_id
+        } # 2번째 추천영화목록을 따옴, 왜? 검색어 안에 들어가야하니까
+        response2 = requests.get(BASE_URL+path2, params2).json() # 추천영화 변수를 만들어주고
+        for i in response2['results']: # 추천영화 안에 정보를 i에 반복시킴
+            results.append(i['title']) # title값만 리스트안에 넣어줌
+        return results
+    else:
+        return None
 
-
-# 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     """
     제목에 해당하는 영화가 있으면 해당 영화의 id를 기반으로 추천 영화 목록 구성
