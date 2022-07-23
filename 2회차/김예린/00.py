c7fdc0c4ebe11fc6609816c6479ef262cfdd0ede
ererink <yelkim0210@gmail.com>
2022-07-23 21:15:59 +0900

    2회차 김예린 프로젝트 제출

--- 2회차/김예린/00.py
+++ 2회차/김예린/00.py
@@ -6,8 +6,8 @@ order_currency = 'BTC'
 payment_currency = 'KRW'
 URL = f'https://api.bithumb.com/public/ticker/{order_currency}_{payment_currency}'
 
-response = requests.get(URL)
+response = requests.get(URL) # URL ?(종목과 원화)정보를 response에 넣는다. 
 
-data = response.json()
+data = response.json() #요청한 json 정보를 data에 넣는다. 
 print(data.get('data').get('closing_price')) 
 # data 딕셔너리에 key로 접근한다. 
