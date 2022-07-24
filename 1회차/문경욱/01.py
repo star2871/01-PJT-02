0a45bc0e07369ab635870b35d5938ac31061145a
9776mk <9776mk@gmail.com>
2022-07-24 21:54:27 +0900

    0724 프로젝트

--- 1회차/문경욱/01.py
+++ 1회차/문경욱/01.py
@@ -1,9 +1,27 @@
 import requests
 
-
 def popular_count():
     pass 
     # 여기에 코드를 작성합니다.  
+    # api_key = 'e5d3a52ec6678bdeec4350297f8456fb'
+    # movie/76341?api_key={api_key}
+    
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/movie/popular'
+    params = {
+        'api_key' : 'e5d3a52ec6678bdeec4350297f8456fb',
+        'language' : 'ko-KR'
+    }
+    response = requests.get(BASE_URL+path, params=params).json()
+    # print(response, type(response))
+
+    movie_list = response.get('results')
+
+    popular_count = 0
+    for movie in movie_list:
+        popular_count += 1
+        
+    return popular_count
 
 
 # 아래의 코드는 수정하지 않습니다.
