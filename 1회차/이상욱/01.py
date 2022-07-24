81a8c4cd809bf840b22a8339f57bbd2acbedb9f2
wxxk <dwde22@gmail.com>
2022-07-24 23:54:31 +0900

    3번까지

--- 1회차/이상욱/01.py
+++ 1회차/이상욱/01.py
@@ -1,11 +1,25 @@
+# 27ff99b8111fb8b63fae9898cd9b455a
+# 27ff99b8111fb8b63fae9898cd9b455a
+# https://api.themoviedb.org/3
 import requests
 
 
 def popular_count():
     pass 
     # 여기에 코드를 작성합니다.  
+    base_url = 'https://api.themoviedb.org/3'
+    path = '/movie/popular'
+    params = {
+        'api_key' : '27ff99b8111fb8b63fae9898cd9b455a',
+        'language' : 'ko-KR'
+    }
 
+    res=requests.get(base_url+path,params=params).json()
+    data = res
+    result = (len(data['results']))
 
+    return result
+    
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     """
